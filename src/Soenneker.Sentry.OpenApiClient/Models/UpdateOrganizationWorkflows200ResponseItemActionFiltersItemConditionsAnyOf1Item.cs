490ada9cdb631faa4c3caf064b676dc64cd17b27
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-08-14 17:56:47 +0000

    Automated update

--- src/Soenneker.Sentry.OpenApiClient/Models/UpdateOrganizationWorkflows200ResponseItemActionFiltersItemConditionsAnyOf1Item.cs
+++ src/Soenneker.Sentry.OpenApiClient/Models/UpdateOrganizationWorkflows200ResponseItemActionFiltersItemConditionsAnyOf1Item.cs
@@ -17,10 +17,10 @@ namespace Soenneker.Sentry.OpenApiClient.Models
         /// <summary>The comparison property</summary>
 #if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
 #nullable enable
-        public global::Soenneker.Sentry.OpenApiClient.Models.UpdateOrganizationWorkflows200ResponseItemActionFiltersItemConditionsAnyOf1ItemComparison? Comparison { get; set; }
+        public global::Soenneker.Sentry.OpenApiClient.Models.UpdateOrganizationWorkflows200ResponseItemActionFiltersItemConditionsAnyOf1Item.UpdateOrganizationWorkflows200ResponseItemActionFiltersItemConditionsAnyOf1Item_comparison? Comparison { get; set; }
 #nullable restore
 #else
-        public global::Soenneker.Sentry.OpenApiClient.Models.UpdateOrganizationWorkflows200ResponseItemActionFiltersItemConditionsAnyOf1ItemComparison Comparison { get; set; }
+        public global::Soenneker.Sentry.OpenApiClient.Models.UpdateOrganizationWorkflows200ResponseItemActionFiltersItemConditionsAnyOf1Item.UpdateOrganizationWorkflows200ResponseItemActionFiltersItemConditionsAnyOf1Item_comparison Comparison { get; set; }
 #endif
         /// <summary>The conditionResult property</summary>
         public bool? ConditionResult { get; set; }
@@ -65,7 +65,7 @@ namespace Soenneker.Sentry.OpenApiClient.Models
         {
             return new Dictionary<string, Action<IParseNode>>
             {
-                { "comparison", n => { Comparison = n.GetObjectValue<global::Soenneker.Sentry.OpenApiClient.Models.UpdateOrganizationWorkflows200ResponseItemActionFiltersItemConditionsAnyOf1ItemComparison>(global::Soenneker.Sentry.OpenApiClient.Models.UpdateOrganizationWorkflows200ResponseItemActionFiltersItemConditionsAnyOf1ItemComparison.CreateFromDiscriminatorValue); } },
+                { "comparison", n => { Comparison = n.GetObjectValue<global::Soenneker.Sentry.OpenApiClient.Models.UpdateOrganizationWorkflows200ResponseItemActionFiltersItemConditionsAnyOf1Item.UpdateOrganizationWorkflows200ResponseItemActionFiltersItemConditionsAnyOf1Item_comparison>(global::Soenneker.Sentry.OpenApiClient.Models.UpdateOrganizationWorkflows200ResponseItemActionFiltersItemConditionsAnyOf1Item.UpdateOrganizationWorkflows200ResponseItemActionFiltersItemConditionsAnyOf1Item_comparison.CreateFromDiscriminatorValue); } },
                 { "conditionResult", n => { ConditionResult = n.GetBoolValue(); } },
                 { "id", n => { Id = n.GetStringValue(); } },
                 { "type", n => { Type = n.GetStringValue(); } },
@@ -78,11 +78,65 @@ namespace Soenneker.Sentry.OpenApiClient.Models
         public virtual void Serialize(ISerializationWriter writer)
         {
             if(ReferenceEquals(writer, null)) throw new ArgumentNullException(nameof(writer));
-            writer.WriteObjectValue<global::Soenneker.Sentry.OpenApiClient.Models.UpdateOrganizationWorkflows200ResponseItemActionFiltersItemConditionsAnyOf1ItemComparison>("comparison", Comparison);
+            writer.WriteObjectValue<global::Soenneker.Sentry.OpenApiClient.Models.UpdateOrganizationWorkflows200ResponseItemActionFiltersItemConditionsAnyOf1Item.UpdateOrganizationWorkflows200ResponseItemActionFiltersItemConditionsAnyOf1Item_comparison>("comparison", Comparison);
             writer.WriteBoolValue("conditionResult", ConditionResult);
             writer.WriteStringValue("id", Id);
             writer.WriteStringValue("type", Type);
             writer.WriteAdditionalData(AdditionalData);
+        }
+        /// <summary>
+        /// Composed type wrapper for classes <see cref="bool"/>, <see cref="int"/>
+        /// </summary>
+        [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
+        public partial class UpdateOrganizationWorkflows200ResponseItemActionFiltersItemConditionsAnyOf1Item_comparison : IComposedTypeWrapper, IParsable
+        {
+            /// <summary>Composed type representation for type <see cref="bool"/></summary>
+            public bool? Boolean { get; set; }
+            /// <summary>Composed type representation for type <see cref="int"/></summary>
+            public int? Integer { get; set; }
+            /// <summary>
+            /// Creates a new instance of the appropriate class based on discriminator value
+            /// </summary>
+            /// <returns>A <see cref="global::Soenneker.Sentry.OpenApiClient.Models.UpdateOrganizationWorkflows200ResponseItemActionFiltersItemConditionsAnyOf1Item.UpdateOrganizationWorkflows200ResponseItemActionFiltersItemConditionsAnyOf1Item_comparison"/></returns>
+            /// <param name="parseNode">The parse node to use to read the discriminator value and create the object</param>
+            public static global::Soenneker.Sentry.OpenApiClient.Models.UpdateOrganizationWorkflows200ResponseItemActionFiltersItemConditionsAnyOf1Item.UpdateOrganizationWorkflows200ResponseItemActionFiltersItemConditionsAnyOf1Item_comparison CreateFromDiscriminatorValue(IParseNode parseNode)
+            {
+                if(ReferenceEquals(parseNode, null)) throw new ArgumentNullException(nameof(parseNode));
+                var result = new global::Soenneker.Sentry.OpenApiClient.Models.UpdateOrganizationWorkflows200ResponseItemActionFiltersItemConditionsAnyOf1Item.UpdateOrganizationWorkflows200ResponseItemActionFiltersItemConditionsAnyOf1Item_comparison();
+                if(parseNode.GetBoolValue() is bool booleanValue)
+                {
+                    result.Boolean = booleanValue;
+                }
+                else if(parseNode.GetIntValue() is int integerValue)
+                {
+                    result.Integer = integerValue;
+                }
+                return result;
+            }
+            /// <summary>
+            /// The deserialization information for the current model
+            /// </summary>
+            /// <returns>A IDictionary&lt;string, Action&lt;IParseNode&gt;&gt;</returns>
+            public virtual IDictionary<string, Action<IParseNode>> GetFieldDeserializers()
+            {
+                return new Dictionary<string, Action<IParseNode>>();
+            }
+            /// <summary>
+            /// Serializes information the current object
+            /// </summary>
+            /// <param name="writer">Serialization writer to use to serialize this model</param>
+            public virtual void Serialize(ISerializationWriter writer)
+            {
+                if(ReferenceEquals(writer, null)) throw new ArgumentNullException(nameof(writer));
+                if(Boolean != null)
+                {
+                    writer.WriteBoolValue(null, Boolean);
+                }
+                else if(Integer != null)
+                {
+                    writer.WriteIntValue(null, Integer);
+                }
+            }
         }
     }
 }
